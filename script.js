c1eb7ecb7611cc6c90e0bb0fde2cc93f40d146b4
Vladyslav Zachepa <112680328+zachepka@users.noreply.github.com>
2024-12-07 00:55:08 -0330

    changes December 7

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('HilleITSchool');
+console.log(typeof 111);
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
+        
+const hillel = 'LOL';
+console.log(hillel.length);
+
+console.log('2'.padStart(2, 0));
+
+console.log(hillel.toLowerCase());
